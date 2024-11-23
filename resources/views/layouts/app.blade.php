6b92886a69f03a69bb7abd3fa55c5dae36543e6d
Mao Rivera <maorivera@outlook.com>
2024-11-22 21:11:58 -0600

    Iniciando proyecto Laravel 9.2 y con tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Devstagram -@yield('sitio')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class='text-4xl font-extrabold'>@yield('titulo')</h1>
+
+<p>@yield('contenido')</p>
+    
+</body>
+</html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('sitio')
+Principal
+@endsection
+@section('titulo')
+Pagina Principal
+@endsection
+
+@section('contenido')
+contenido principal
+@endsection
